772604262580647ab9e27833d5581d42770f65a2
yeonkyoungpark <yyeon111@naver.com>
2023-03-06 18:50:09 +0900

    sort for NaN

---
+++ practice/sort.js
@@ -0,0 +1,11 @@
+console.log(
+  [4n, 7, 6n, 3, 1, 5, 9, 2n, 8n]
+  .sort((a, b) => a > b ? 1 : -1)
+);
+
+// console.log(
+//   [4n, 7, 6n, 3, 1, 5, 9, 2n, 8n]
+//   .sort((a, b) => a-b)
+// );
+
+console.log([1, 2, 3, 4, 5].sort((a,b) => a < b ? 1 : -1));
