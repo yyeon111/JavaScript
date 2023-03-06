3c0eb0e5607a82e67c083d9fb2494e31a564096f
yeonkyoungpark <yyeon111@naver.com>
2023-03-06 18:57:44 +0900

    BigInt for Boolean

---
+++ practice/boolean.js
@@ -0,0 +1,17 @@
+console.log(
+  !!(0n),
+  !!(1n)
+);
+
+console.log(
+  5n / 2n
+);
+
+// console.log(
+//   Math.max(1n, 2n)
+// );
+
+console.log(
+  Number(1n),
+  Number(9007199254740993n)
+);
