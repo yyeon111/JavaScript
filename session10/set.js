357058058bb1bfa7e905ebc82e5a500d1c6d7a11
yeonkyoungpark <yyeon111@naver.com>
2023-03-07 12:03:18 +0900

    new Set

--- session10/set.js
+++ session10/set.js
@@ -1,62 +1,63 @@
-//set 배열처럼 여러가지를 담을 수 있는 주머니
-//동일한 값을 여러번 포함할 수 없음
-//값들의 순서가 무의미함
-//new Set을 통해 새로운 객체를 생성할 수 있다.
-const set1= new Set();
+//표준 내장객체 
+// Set 생성
+const set1 = new Set(); //object
+
+// add 메서드 - 요소 추가
 set1.add(1);
 set1.add('A');
 set1.add(true);
 
 console.log(set1);
+console.log(set1[2]); //인덱스로 접근 불가능
 
-//배열처럼 보이지만 배열이 아니다. index로 접근 안됨
-//이미 포함된 요소는 추가되지 않음.
-//이미 있는거 추가한다고 오류도 나지 않는다.
 
-//배열 인자를 넣으면 set생성, 초기화 할수 있다.
-//중복 된 값이 있을 시 알아서 제거된다.
-const set2 = new Set([1,1,1,'A',true]);
+const testString = 'hello';
+console.log(testString[0]);
+
+const set2 = new Set([1, 1, 1, 'A', true]); //Set에 넣을 요소들을 배열로 넣어준다.
+console.log(set2); //{1, 'A', true}
+// const set3 = new Set(1, 2, 3);
+//Set instance method add
+set2.add(false);
 console.log(set2);
-
-
-//특정한 값이 들어있는지 확인할 때 유용하다. has -> true, false로 반환
-console.log(
-  set2.has(1),
-  set2.has('A'),
-  set2.has(4),
-);
-
-//요소 제거와 성공 여부를 동시에 알 수 있다 delete -> true fasle로 반환
-
-console.log(
-  set2.delete('A'),
-  set2.delete(true),
-  set2.delete(100),
-);
-
+//Set instance method has
+console.log(set2.has(false));
+//Set instance method delete()
+console.log(set2.delete(false));
 console.log(set2);
-
-//set의 길이는 size로 알 수 있다.
+//Set instance property
 console.log(set2.size);
-//모든 요소들 삭제.
+//Set instance method clear()
 set2.clear();
 console.log(set2);
 
-//배열을 넣으면 이터러블로서 set을 사용 할 수 있다.
-//for of문을 사용 할 수 있다.
-//스프레드 문법이 가능하다.
+const objStr = new Set()
+// .add({x: 1, y: 2})
+// // .add({x: 1, y: 2})
+// // .add([1, 2, 3])
+.add([1, 2, 3]); //add로 참조형 데이터를 넣으면 그대로 들어간다. 
+//생성자 함수에서 배열로 넣으면 펼쳐져서 들어감.
 
-const set3 = new Set(['a','b','c','d','e']);
-for(s of set3){
-  console.log(s);
-};
+console.log(objStr);
 
-const set = [...set3]; //스프레드문법
-console.log(set);
+// const testobj = new Set({x: 1, y:2});
+// console.log(testobj);
 
-//중복된 것을 빼고 스프레드로 만드는 방법
-const arr1 = [1,1,1,2,2,3,4,4,5];
-const arr2 = [...new Set(arr1)];
-console.log(arr2);
+//iterable for of문
+const arr = ['A', 'B', 'C', 'D', 'E'];
+const set = new Set(arr); //펼쳐져서 들어옴
 
-set.forEach(console.log);
+//배열로 바꾸거나 할 필요 없이 Set 자체가 for of문 가능하다.
+for(itm of set){
+  console.log(itm);
+}
+
+//sperad
+const newArr = [...set];
+console.log(newArr); //배열로서 다시 가져 올 수 있다.
+
+const arr1 = [1, 1, 1, 2, 2, 3, 4, 5]; //스프레드는 객체, 배열 안에 있는 것들을 다 가져 올 수 있기 때문에
+const arr2 = [...new Set(arr1)];//[...{1, 2, 3, 4, 5}] => [1, 2, 3, 4, 5]
+
+//forEach문 가능
+set.forEach((itm, idx, setObj) => console.log(itm, idx, setObj));
