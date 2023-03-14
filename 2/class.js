da09226fad5654f4c617a053f8580782b4667fd4
yeonkyoungpark <yyeon111@naver.com>
2023-03-15 01:20:14 +0900

    class ES6, parameter, argument test

---
+++ 2/class.js
@@ -0,0 +1,28 @@
+
+
+// function Student (name, age, school) {
+//   this.name = name;
+//   this.age = age;
+//   this.school = school;
+// }
+// const name = 'Nora';
+// const age = 20;
+// const school = 'I University';
+
+// const a = new Student('YS', 22, 'A University');
+// console.log(a);
+
+class Student{
+  constructor () {
+    this.name = name;
+    this.age = age;
+    this.school = school;
+  }
+}
+
+const name = 'Nora';
+const age = 20;
+const school = 'I University';
+
+const a = new Student('YS', 22, 'A University');
+console.log(a);
