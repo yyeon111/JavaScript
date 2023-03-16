c5a9b5d07433fde3f1c3f4cd439209f67d21ab08
yeonkyoungpark <yyeon111@naver.com>
2023-03-17 01:44:55 +0900

    할당 연산자

---
+++ 3/add.js
@@ -0,0 +1,20 @@
+// const numbers1 = [1, 2, 3, 4, 5]; //obj의 내부 변동이기 때문에 const
+// const numbers2 = [];
+
+// for (let i = 0; i < numbers1.length ; i++) {
+// 	numbers1[i]++;
+//   console.log(numbers1[i]);
+// }
+
+// let a = 1;
+// a++;
+// a;
+// console.log(a);
+
+// let b = 1;
+// ++b;
+// console.log(b);
+
+let a = 1, b;
+b = a++;
+console.log(a,b);
