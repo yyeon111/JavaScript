21839692241e0bdd7677ed0c9fb495c0c25a0d70
yeonkyoungpark <yyeon111@naver.com>
2023-03-17 00:45:06 +0900

    제어문 정리

--- 3/while.js
+++ 3/while.js
@@ -1,28 +1,29 @@
-// let x = 0;
-// while (x < 14) {
-//   const toContinue = x % 2 === 0; //0  2  4
-//   const toBreak = x > 7; // 0  2  4
-//   const xNow = x++; // 0  2  4
+let x = 0;
+while (x < 14) {
+  const toContinue = x % 2 === 0; //0  2  4
+  const toBreak = x > 7; // 0  2  4
+  const xNow = x++; // 0  2  4
 
-//   if (toContinue) continue; //1  3  5
-//   if (toBreak) break; // 1  3  5
+  if (toContinue) continue; //1  3  5
+  if (toBreak) break; // 1  3  5
 
-//   console.log(xNow); // 1  3  5
-// }
+  console.log(xNow); // 1  3  5
+}
 
 
-// //할당이 되거나, 직접적 사용이 될 때만 count x++
-// let x = 0;
-// while (x < 14) { //0 2 4
-//   const xNow = x++; //0 2 4
+//할당이 되거나, 직접적 사용이 될 때만 count x++
+let x = 0;
+while (x < 14) { //0 2 4
+  const xNow = x++; //0 2 4
 
-//   if (xNow % 2 === 0) continue; //1 3 5
-//   if (xNow > 7) break; //1 3 5
+  if (xNow % 2 === 0) continue; //1 3 5
+  if (xNow > 7) break; //1 3 5
 
-//   console.log(xNow); //1 3 5
-// }
+  console.log(xNow); //1 3 5
+}
 
 let x = 0;
 do {
   console.log(x++);
 } while (x < 10) ;
+
---
+++ 3/x.js
@@ -0,0 +1,8 @@
+let x = 0;
+let test = x++;
+console.log(x);
+
+console.log(x++);
+console.log(x);
+
+//다음루프 돌 때
