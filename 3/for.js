a2a2f5c6fbae01b244ce2f7e8f06d18c38e9d620
yeonkyoungpark <yyeon111@naver.com>
2023-03-16 21:18:53 +0900

    for

---
+++ 3/for.js
@@ -0,0 +1,74 @@
+// for(let x = 0, y = 10; x <= y; x++, y--){
+//   console.log(x, y); //next loop
+// }
+
+for (
+  let x = true, y = 0, z = 0;
+  y * z < 10; 
+  x = !x, x ? y++ : z++ //finial expression
+) {
+  console.log(y, z);
+}
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i--);
+// }
+
+// console.log('출력 안됨');
+
+const lunch = {
+  name: '라면',
+  taste: '매운맛',
+  kilocalories: 500,
+  cold: false
+}
+
+for (const key in lunch) { // 💡 변할 것이 아니므로 const 사용
+  console.log(key, ':', lunch[key])
+}
+
+console.log('name' in lunch);
+
+const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers2 = [];
+
+for (let num of numbers1) {
+  num++; // ⚠️ 복사된 값. let 사용 주목
+  numbers2.push(num + 1);
+}
+console.log(numbers1, numbers2); //참조(어딘가에)
+
+for (let i = 0; i < numbers1.length; i++) {
+  numbers1[i]++; // ⚠️ 실제 값에 인덱스로 접근 - 원본 배열의 내용 변경
+}
+console.log(numbers1, numbers2);
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 3 === 0) continue;
+  console.log(i);
+}
+
+console.log('for 루프 종료');
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) break;
+  console.log(i);
+}
+
+console.log('for 루프 종료');
+
+outer:
+for (let i = 1; i < 10; i++) {
+
+  inner:
+  for (let j = 1; j < 10; j++) {
+
+    if (j % 2 === 0) continue inner;
+    if (i * j >= 30) continue outer;
+    
+    if (j > 8) break inner;
+    if (i - j > 7) break outer;
+
+    console.log(i, j, i * j);
+  }
+}
