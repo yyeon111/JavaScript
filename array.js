d6b5f14103b899f49d25efbb918c3a328253554e
yeonkyoungpark <yyeon111@naver.com>
2023-03-20 16:29:47 +0900

    checked rest parameters and arguments in arrow function

--- array.js
+++ array.js
@@ -1,24 +1,35 @@
-const argu = function() {
-console.log(arguments);
-console.log(typeof arguments);
-console.log(arguments instanceof Array);
-console.log(arguments instanceof Object);
+// const argu = function() {
+// console.log(arguments);
+// console.log(typeof arguments);
+// console.log(arguments instanceof Array);
+// console.log(arguments instanceof Object);
 
-for(const i of arguments){
-  console.log(i);
-}
-}
+// for(const i of arguments){
+//   console.log(i);
+// }
+// }
 
-const test1 = argu(1, 2, 3, 4, 5);
-console.log(test1);
+// const test1 = argu(1, 2, 3, 4, 5);
+// console.log(test1);
 
 
-const argument = function(param, ...params) {
-  console.log(params);
-  console.log(typeof params);
-  console.log(params instanceof Array);
-  console.log(params instanceof Object);
-  }
+// const argument = function(param, ...params) {
+//   console.log(params);
+//   console.log(typeof params);
+//   console.log(params instanceof Array);
+//   console.log(params instanceof Object);
+//   }
   
-  const test2 = argument(1, 2, 3, 4, 5);
-  console.log(test2);
+  // const test2 = argument(1, 2, 3, 4, 5);
+  // console.log(test2);
+
+  const argument = (param, ...params)  => {
+    console.log(arguments);
+    console.log(params);
+    console.log(typeof params);
+    console.log(params instanceof Array);
+    console.log(params instanceof Object);
+    }
+    
+    const test2 = argument(1, 2, 3, 4, 5);
+    console.log(test2);
