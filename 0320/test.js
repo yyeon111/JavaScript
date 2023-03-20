fa72cd9c5eb5e804846e19ebf39512ccabed69c5
yeonkyoungpark <yyeon111@naver.com>
2023-03-20 23:57:11 +0900

    tested class w/ encapsulation

---
+++ 0320/test.js
@@ -0,0 +1,10 @@
+// function A () {
+//   this.#name = name;
+//   this.#age = age;
+//   this.#introduce = function () {
+//     return `${this.#name}의 나이는 ${this.#age} 입니다.`;
+//   }
+// }
+
+// const a = new A ('Nora', 18);
+// console.log(a);
