ab3c5cc221a8bf47d0d926d0936cedb729225d22
yeonkyoungpark <yyeon111@naver.com>
2023-03-20 15:26:02 +0900

    checked instance

---
+++ 0320.js
@@ -0,0 +1,22 @@
+const str = 'hello';
+const num = 1;
+const arr = [];
+const obj = {};
+
+console.log('==============typeof================')
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof arr);
+console.log(typeof obj);
+
+console.log('==============instanceof Object================')
+console.log(str instanceof Object);
+console.log(num instanceof Object);
+console.log(arr instanceof Object);
+console.log(obj instanceof Object);
+
+console.log('==============instacneof typeof================')
+console.log(str instanceof String);
+console.log(num instanceof Number);
+console.log(arr instanceof Array);
+console.log(obj instanceof Object);
