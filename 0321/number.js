797329d44a88a8cfba6608d6f6cc2fef7c8817e8
yeonkyoungpark <yyeon111@naver.com>
2023-03-21 18:03:55 +0900

    compare isNaN and Number.isNaN

---
+++ 0321/number.js
@@ -0,0 +1,8 @@
+console.log(Number.isNaN('1')); //숫자다
+console.log(Number.isNaN('A')); //숫자다 
+console.log(isNaN('1')); //숫자다.
+console.log(isNaN('A')); //숫자가 아니다.
+console.log(Number.isNaN(false));
+console.log(isNaN(false));
+console.log(Number.isNaN(true));
+console.log(isNaN(true));
