4b597737ac78197dcc178e4f9534b3bc220bfa3d
yeonkyoungpark <yyeon111@naver.com>
2023-03-22 00:47:46 +0900

    Checked how to use new Date function w/ static methods, ptotype methods

--- 0321/date.js
+++ 0321/date.js
@@ -1,2 +1,69 @@
-const nowStr = Date();
-console.log(typeof nowStr, nowStr);
+// const nowStr = Date();
+// console.log(typeof nowStr, nowStr);
+// const test = new Date();
+// console.log(typeof test, test);
+// // console.log(typeof test.getHours());
+// // const test1 = new Date(0);
+// // console.log(typeof test1, test1);
+
+// // const num = new Number(1);
+// // console.log(typeof num, num instanceof Number);
+
+// // console.log(new Date().getTime());
+
+// console.log(new Date(new Date().getTime()));
+
+// console.log(new Date(Date.now()));
+
+// console.log(typeof Date.now());
+
+// console.log(Date.parse('March 21, 2023 00:00:00 UTC')); //1970 - 01-01 00:00:00
+// console.log(Date.parse('March 22, 2023 02:00:00'));
+// console.log(Date.UTC(2023, 2, 21, 0, 0, 0));
+
+// const test3 = new Date(2023, 2, 21, 9, 0, 0);
+// console.log(test3);
+// console.log(new Date(Date.UTC(2023, 2, 21, 0, 0, 0)));
+
+// console.log(test.toString());
+// console.log(test.toDateString());
+// console.log(test.toTimeString());
+
+// console.log(test.toLocaleString());
+// console.log(Date());
+// console.log(test.getFullYear());
+
+// console.log(new Date(2022, 2).toString());
+// test.setYear(2022);
+// test.setMonth(2);
+// test.setDate(1);
+// console.log(test.toString());
+
+// console.log(new Date(2022, 3, 1, 0, 0, 0).getTime());
+
+
+// const obj = new Date(2022, 3, 1, 0, 0, 0);
+// console.log()
+// console.log(new Date(2022, 3, 1).toString());
+
+// console.log(Date.UTC(2022, 5, 1));
+// const obj2 = new Date(2022, 5, 1);
+// console.log(obj2.toString());
+// console.log(Date.UTC(2022, 5, 1)); //더 많이 지남
+// console.log(new Date(Date.UTC(2022, 5, 1)));
+
+// const obj3 = obj2.getTime();
+// console.log(obj3);
+// console.log(new Date(obj3));
+// console.log(new Date(obj3));
+// console.log(obj2.getTime());
+// console.log(new Date(Date.UTC(2022, 5, 1)));
+// console.log(new Date(Date.parse('Jun 1, 2022 09:00:00 UTC')))
+// console.log(new Date(obj2.getTime()));
+
+// console.log(new Date().toString());
+// console.log(new Date().getTime());
+// console.log(new Date(new Date().getTime()).toString());
+
+console.log(Date.now());
+console.log(new Date().getTime());
