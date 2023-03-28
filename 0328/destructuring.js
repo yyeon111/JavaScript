34469f5eb106fc3fe39c21e763de6e2549c60e30
yeonkyoungpark <yyeon111@naver.com>
2023-03-28 19:30:17 +0900

    for TIL

--- 0328/destructuring.js
+++ 0328/destructuring.js
@@ -13,18 +13,50 @@
 // const [o, p, ...rested] = arr;
 // console.log(o, p, rested);
 
-const arr = ['A', 'B', 'C', 'D', 'E'];
-const set = new Set(arr);
-const [a, b, ...rest] = set;
+// const arr = ['A', 'B', 'C', 'D', 'E'];
+// const set = new Set(arr);
+// const [a, b, ...rest] = set;
 
-console.log(a);
-console.log(b);
-console.log(rest);
+// console.log(a);
+// console.log(b);
+// console.log(rest);
 
-function test ([a, ...b]) {
-  console.log(a, b);
-}
+// function test ([a, ...b]) {
+//   console.log(a, b);
+// }
 
-test(arr);
+// test(arr);
 
-set.forEach(i => console.log(i));
+// set.forEach(i => console.log(i));
+
+// const arr = new Set([1, 2, 3, 4]);
+// // added.a = 0;
+// // console.log(added);
+// // added.push(0);
+// // console.log(added);
+// added.add({x:1, y:2});
+// console.log(added.keys());
+// console.log(added.entries());
+
+
+// const arr = new Set([1, 2, 3, 4]);
+// const [x, y, ...z] = arr;
+// console.log(x, y, z); // 1 2 [3, 4]
+
+// const obj = {a: 1, b: 2, c: 3, d: 4};
+// const {a, c, ...rest} = obj;
+// console.log(a, c, rest);// 1 3 {b: 2, d: 4}
+
+const arr = [1, 2, 3];
+delete arr[1];
+console.log(arr);
+
+const obj = {x: 1, y: 2, z: 3};
+delete obj['y'];
+console.log(obj);
+
+arr.unshift(0);
+console.log(arr);
+
+arr.splice(2,0,2);
+console.log(arr);
