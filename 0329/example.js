21bfc4700f56fed5638259f123b7e6d8a05a97c7
yeonkyoungpark <yyeon111@naver.com>
2023-03-29 23:10:59 +0900

    final ver

--- 0329/example.js
+++ 0329/example.js
@@ -11,24 +11,24 @@ for(const itm of diceTenTimes()){
   console.log(itm);
 }
 
-function* genFunction () {
-  yield 1;
-  yield 2;
-  yield 3;
-  yield 4;
-  yield 5;
-}
+// function* genFunction () {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+//   yield 4;
+//   yield 5;
+// }
 
-let genObj = genFunction();
-// 로그 펼쳐서 살펴볼 것
-console.log( genObj.next());
-console.log( genObj.next());
-console.log( genObj.next());
-console.log( genObj.next());
-console.log( genObj.next());
-console.log( genObj.next());
-console.log( genObj.next());
-console.log( genObj.next());
-console.log( genObj.next());
-console.log([...genObj]);
-console.log( genObj.next());
+// let genObj = genFunction();
+// // 로그 펼쳐서 살펴볼 것
+// console.log( genObj.next());
+// console.log( genObj.next());
+// console.log( genObj.next());
+// console.log( genObj.next());
+// console.log( genObj.next());
+// console.log( genObj.next());
+// console.log( genObj.next());
+// console.log( genObj.next());
+// console.log( genObj.next());
+// console.log([...genObj]);
+// console.log( genObj.next());
