f144ccb72c16c9a7d30c33ff6dc8bf03f37e67d8
yeonkyoungpark <yyeon111@naver.com>
2023-03-30 17:58:02 +0900

    generator

---
+++ 0330/generator.js
@@ -0,0 +1,39 @@
+function* genFunction () {
+  console.log('하나를 반환합니다.');
+  yield '하나';
+
+  console.log('둘을 반환합니다.');
+  yield '둘';
+
+  console.log('셋을 반환합니다.');
+  yield '셋';
+}
+
+const genFunc = genFunction();
+
+console.log(genFunc); //generator [Symbol.iterator], next()
+
+
+
+  console.log(genFunc.next());
+  console.log(genFunc.next());
+
+  function* generator () {
+    yield 1;
+    yield 2;
+    yield 3;
+    yield 4;
+    yield 5;
+  }
+
+  let genObj = generator();
+
+  console.log(genObj.next());
+  console.log([...generator()]);
+  for (const i of generator()) {
+    console.log(i);
+  }
+
+  for(i = 1; i < 10; i++) {
+    console.log(genObj.next());
+  }
