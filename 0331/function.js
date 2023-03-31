cae2fd3d69491e218f874dbf92a67b205828fe1e
yeonkyoungpark <yyeon111@naver.com>
2023-03-31 23:46:23 +0900

    try catch finally

---
+++ 0331/function.js
@@ -0,0 +1,20 @@
+function connect () {console.log(`🤩 Connect!`);}
+function discconect () {console.log(`🤗 Disconnect!`);}
+function sendArray (arr) {console.log(`😎 Send!`, arr);}
+
+function sendStringAsArray(str) {
+  connect();
+  try {
+    sendArray(str.split(''));
+    return true;
+  } catch (e) {
+    console.error('❗Check this Error', e);
+    return false;
+  } finally {
+     discconect();
+  }
+ 
+}
+
+// sendStringAsArray('ABC') ? '[성공]' : '[실패]';
+['ABC', '가나다', 123, '123'].forEach(i => console.log(sendStringAsArray(i) ? '[성공]' : '[실패]'))
